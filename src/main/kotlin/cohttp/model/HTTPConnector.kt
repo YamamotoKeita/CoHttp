ee48f3c4415dba59f0ece482f1e9fdf8c62b9fd7
山本敬太 <yamamoto@altonotes.co.jp>
2022-12-01 10:31:16 +0900

    Add comments

--- src/main/kotlin/cohttp/model/HTTPConnector.kt
+++ src/main/kotlin/cohttp/model/HTTPConnector.kt
@@ -1,6 +1,8 @@
 package cohttp.model
 
 /**
+ * TODO Now HTTPConnector launch and cancel the coroutine, but instead, CoHttp should do it. Then this interface can be more simple.
+ *
  * A module to execute HTTP communication.
  * An HTTPConnector is assumed to be created each HTTP communication.
  * You can implement a custom HTTPConnector and apply it via HTTPContext.makeHTTPConnector.
